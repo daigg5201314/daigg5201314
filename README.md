d1a0dc35b69bf68b0cc5be89dc4b5d4fa6a24a34
戴哥哥 <daibaoye520@outlook.com>
2023-07-30 18:26:55 +0800

    fix：优化了一些选项。具体请看提交记录和README

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 1、测试通过  --ok
 2、编写Makefile脚本进行编译  --ok
 3、编写shell脚本进行编译  --ok
+4、优化Makefile编译选项，对固件进行检查 --ok
+5、优化shell脚本编译选项，对固件进行检查 --flase
+
 
 
--- build.sh
+++ build.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 echo -e "Windows中文显示异常解决"
+export LANG=zh_CN.UTF-8
 CHCP 65001
 
 echo -e "当前目录: $(pwd)"
 
-echo -e "清理编译缓存"
+echo -e "检查是否需要清理缓存。\r\n"
+make check_clean
+if [ $? -eq 0 ]; then
+    echo -e "编译缓存已清理。\r\n"
+else
+    echo -e "编译缓存未清理。\r\n"
+    echo -e "清理编译缓存。\r\n"
 make clean
+fi
+
+
 
 current_date=$(date +"%Y-%m-%d")
 current_time=$(date +"%H:%M:%S")
 echo -e "当前日期: $current_date"
 echo -e "当前时间: $current_time"
 
-echo -e "开始编译"
+echo -e "开始编译。\r\n"
 make all
 
 echo -e "编译完成"
